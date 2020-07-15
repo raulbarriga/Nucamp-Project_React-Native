838149c18aeef771e99d552aa530cb15749cf8ef
Raul Barriga <barrigaraulaug@gmail.com>
2020-07-15 12:36:54 -0700

    Animations

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
-import Loading from './LoadingComponent';
+import React, { Component } from "react";
+import { View, Text, Animated } from "react-native";
+import { Card } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         campsites: state.campsites,
         promotions: state.promotions,
-        partners: state.partners
+        partners: state.partners,
     };
 };
 
 function RenderItem(props) {
-    const {item} = props;
+    const { item } = props;
 
     if (props.isLoading) {
         return <Loading />;
@@ -30,11 +30,8 @@ function RenderItem(props) {
         return (
             <Card
                 featuredTitle={item.name}
-                image={{uri: baseUrl + item.image}}>
-                <Text
-                    style={{margin: 10}}>
-                    {item.description}
-                </Text>
+                image={{ uri: baseUrl + item.image }}>
+                <Text style={{ margin: 10 }}>{item.description}</Text>
             </Card>
         );
     }
@@ -42,30 +39,60 @@ function RenderItem(props) {
 }
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scaleValue: new Animated.Value(0),
+        };
+    }
+
+    animate() {
+        Animated.timing(this.state.scaleValue, {
+            toValue: 1,
+            duration: 1500,
+        }).start();
+    }
+
+    componentDidMount() {
+        this.animate();
+    }
 
     static navigationOptions = {
-        title: 'Home'
-    }
+        title: "Home",
+    };
 
     render() {
         return (
-            <ScrollView>
+            <Animated.ScrollView
+                style={{ transform: [{ scale: this.state.scaleValue }] }}>
                 <RenderItem
-                    item={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                    item={
+                        this.props.campsites.campsites.filter(
+                            (campsite) => campsite.featured
+                        )[0]
+                    }
                     isLoading={this.props.campsites.isLoading}
                     errMess={this.props.campsites.errMess}
                 />
                 <RenderItem
-                    item={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
+                    item={
+                        this.props.promotions.promotions.filter(
+                            (promotion) => promotion.featured
+                        )[0]
+                    }
                     isLoading={this.props.promotions.isLoading}
-                    errMess={this.props.promotions.errMess} 
+                    errMess={this.props.promotions.errMess}
                 />
                 <RenderItem
-                    item={this.props.partners.partners.filter(partner => partner.featured)[0]}
+                    item={
+                        this.props.partners.partners.filter(
+                            (partner) => partner.featured
+                        )[0]
+                    }
                     isLoading={this.props.partners.isLoading}
-                    errMess={this.props.partners.errMess} 
+                    errMess={this.props.partners.errMess}
                 />
-            </ScrollView>
+            </Animated.ScrollView>
         );
     }
 }
